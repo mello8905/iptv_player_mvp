4fd4e57c90fced44ca476e731eb52e8619e2fa90
mello8905 <demellomedeiros53@gmail.com>
2025-11-08 09:56:12 -0300

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'screens/login_screen.dart';
+import 'screens/playlist_screen.dart';
+import 'screens/player_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'IPTV Player',
+      theme: ThemeData(primarySwatch: Colors.red),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const LoginScreen(),
+        '/playlist': (context) => const PlaylistScreen(),
+        '/player': (context) => const PlayerScreen(),
+      },
+    );
+  }
+}
