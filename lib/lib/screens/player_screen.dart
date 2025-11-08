c1818b9fd8b0a77a4d85015df13ca6cb3ad70c4e
mello8905 <demellomedeiros53@gmail.com>
2025-11-08 11:57:47 -0300

    Create player_screen.dart

---
+++ lib/lib/screens/player_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class PlayerScreen extends StatelessWidget {
+  const PlayerScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Tela do player IPTV'),
+      ),
+    );
+  }
+}
