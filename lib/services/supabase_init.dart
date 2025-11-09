8c94ff084975bc936ab2e3872a87c2da2c1dfbbc
mello8905 <demellomedeiros53@gmail.com>
2025-11-08 23:14:20 -0300

    Create supabase_init.dart

---
+++ lib/services/supabase_init.dart
@@ -0,0 +1,13 @@
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+const String supabaseUrl = https://udjiwjcngkecfhxwrtwu.supabase.co';
+const String supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVkaml3amNuZ2tlY2ZoeHdydHd1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI2MzUzODcsImV4cCI6MjA3ODIxMTM4N30.2rs2gZZmBAQeesqDJlG1m_TqMdvWBX6tWp90Urjt9HQ';
+
+Future<void> initSupabase() async {
+  await Supabase.initialize(
+    url: supabaseUrl,
+    anonKey: supabaseAnonKey,
+  );
+}
+
+final supa = Supabase.instance.client;
