f79ec2688270e8359b401da0ded8cfbc33ed7b2a
mello8905 <demellomedeiros53@gmail.com>
2025-11-08 23:50:46 -0300

    Update supabase_init.dart

--- lib/services/supabase_init.dart
+++ lib/services/supabase_init.dart
@@ -1,7 +1,8 @@
+
 import 'package:supabase_flutter/supabase_flutter.dart';
 
-const String supabaseUrl = https://''udjiwjcngkecfhxwrtwu.supabase.co'';
-const String supabaseAnonKey = ''eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVkaml3amNuZ2tlY2ZoeHdydHd1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI2MzUzODcsImV4cCI6MjA3ODIxMTM4N30.2rs2gZZmBAQeesqDJlG1m_TqMdvWBX6tWp90Urjt9HQ'';
+const String supabaseUrl = "https://udjwimjcnekxtww.supabase.co";
+const String supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVkaml3amNuZ2tlY2ZoeHdydHd1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI2MzUzODcsImV4cCI6MjA3ODIxMTM4N30.2rs2gZZmBAQeesqDJlG1m_TqMdvWBX6tWp90Urjt9HQ";
 
 Future<void> initSupabase() async {
   await Supabase.initialize(
@@ -9,5 +10,3 @@ Future<void> initSupabase() async {
     anonKey: supabaseAnonKey,
   );
 }
-
-final supa = Supabase.instance.client;
